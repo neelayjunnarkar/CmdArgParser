f5519e887cdc60c08ed095029e14cf1b8dd2f5f2
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2016-02-23 16:46:14 -0800

    rewhitespaced Parser.cpp

--- Parser.hpp
+++ Parser.hpp
@@ -11,17 +11,17 @@ public:
 	Parser(int argc, char **argv);
 
 	void add_arg(std::string longhand, std::vector<Constraint> constraints={}, std::string shorthand="", bool required = false, std::string description="");
-	
-    bool check_validity(std::string *const err_msg = nullptr);
+		
+	bool check_validity(std::string *const err_msg = nullptr);
 
-    template <typename T>
-    T get(std::string hand) const;
+	template <typename T>
+	T get(std::string hand) const;
     
-    template <typename T>
-    T get_shorthand(std::string shorthand) const;
+	template <typename T>
+	T get_shorthand(std::string shorthand) const;
     
-    template <typename T>
-    T get_longhand(std::string longhand) const;
+	template <typename T>
+	T get_longhand(std::string longhand) const;
      
 	std::string get_raw() const;
 private:
@@ -31,8 +31,8 @@ private:
 		std::string shorthand;
 		std::string longhand;
 		std::string description;
-        bool required;        
-        std::vector<std::string> arguments; //arguments to the argument to the program
+		bool required;        
+		std::vector<std::string> arguments; //arguments to the argument to the program
 	};
 
 	std::vector<std::string> _raw_args;
