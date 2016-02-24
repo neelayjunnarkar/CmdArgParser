93e0b2f1627618c8265b189b5a5592a086a41c82
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2016-02-23 17:01:47 -0800

    rewhitespaced main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include "Parser.hpp"
 #include <string>
 #include <sstream>
+
 int main(int argc, char **argv) {
 	std::cout << "Passing arguments to parser... " << std::endl;
 	Parser parser(argc, argv);
-    parser.add_arg("longhand", {}, "S", "THIS IS AN ARGUMENT");
+	parser.add_arg("longhand", {}, "S", "THIS IS AN ARGUMENT");
 	std::cout << parser.get_raw() << std::endl;
-    return 0;
+	return 0;
 }
