0df82967ef93f3e2136580fa385e2ceb9b11899a
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2016-04-02 08:34:46 -0700

    Arg and _Arg cstors

--- Parser.cpp
+++ Parser.cpp
@@ -1,18 +1,41 @@
 #include "Parser.hpp"
 
+template <typename T>
+Parser::Arg<T>::Arg(std::string internal_id, std::vector<int> poss, std::vector<Constraint> cstrts, bool req, std::string desc) :
+    _Arg({internal_id}, desc),
+    poss{new std::vector<int>{poss}},
+    cstrts{new std::vector<Constraint>{cstrts}},
+    req{new bool{req}} {      
+}
+template <typename T>
+Parser::Arg<T>::Arg(std::string lh, std::string sh, std::string desc) :
+    _Arg({lh, sh}, desc),
+    poss{nullptr},
+    cstrts{nullptr},
+    req{nullptr} {
+}
+template <typename T>
+Parser::Arg<T>::Arg(std::string lh, std::string sh, std::vector<Constraint> cstrts, bool req, std::string desc) :
+    _Arg({lh, sh}, desc),
+    poss{nullptr},
+    cstrts{new std::vector<Constraint>{cstrts}},
+    req{new std::vector<Constraint>{req}} {
+}
+
+
 Parser::Parser(int argc, char **argv) {
 	for (int i = 1; i < argc; ++i) {
 		_raw_args.push_back(std::string{argv[i]});
 	}
 }
 
-bool Parser::check_validity() const {
+bool Parser::valid() const {
 
 	return true;
 }
 
 void Parser::parse() {
-
+    
 }
 
 std::string Parser::get_raw() const {
--- Parser.hpp
+++ Parser.hpp
@@ -15,9 +15,12 @@
 class Parser {
 private:
 	struct _Arg {
-		std::string name;
+        const int lh = 0; // if name.size == 2
+        const int sh = 1; // if name.size() == 2
+        const int internal_id = 0; // if name.size() == 1
+		std::vector<std::string> name;
 		std::string desc;
-		_Arg(const std::string &name, const std::string &desc) :
+		_Arg(const std::vector<std::string> &name, const std::string &desc) :
 			name{name},
 			desc{desc} { 
 		}
@@ -25,14 +28,21 @@ private:
 	};
 	template <typename T>
 	struct Arg : _Arg {
-		
-	}
-	
+        std::vector<int> *poss;
+        std::vector<Constraint> *cstrts;
+        bool *req;
+        Arg(std::string internal_id, std::vector<int> poss, std::vector<Constraint> cstrts, bool req, std::string desc);
+        Arg(std::string lh, std::string sh, std::string desc);
+        Arg(std::string lh, std::string sh, std::vector<Constraint> cstrts, bool req, std::string desc);
+        std::vector<T> arg_vals;
+	};
+
 	std::vector<std::string> _raw_args;
+    std::vector<_Arg*> _args;
 public:
 	Parser(int argc, char **argv);
 
-	void set_positional(std::vector<int> poss, std::vector<Constraint> cstrts, bool req, std::string desc) const;
+	void set_positional(std::string internal_id, std::vector<int> poss, std::vector<Constraint> cstrts, bool req, std::string desc) const;
 	void set_bool(std::string lh, std::string sh, std::string desc) const;
 	void set_labeled(std::string lh, std::string sh, std::vector<Constraint> cstrts, bool req, std::string desc) const;
 	/*
@@ -40,7 +50,7 @@ public:
 	 * 	Arguments pass constraints
 	 *	All required arguments are there
 	 */
-	bool check_validity() const;
+	bool valid() const;
 
 	void parse();
 
